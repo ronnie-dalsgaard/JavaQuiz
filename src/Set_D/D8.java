7d7ef7d2ffaae4d36c28cbc3eac9dba5ae69cb9f
Ronnie Dalsgaard <ronnie.dalsgaard@gmail.com>
2016-12-02 11:55:49 +0100

    init

---
+++ src/Set_D/D8.java
@@ -0,0 +1,10 @@
+package Set_D;
+
+public class D8 {
+
+    public static void main(String[] args) {
+        for(int i = 0x9; i < 13; i++){
+            System.out.print(Integer.toHexString(i+1)+", ");
+        }
+    }
+}
